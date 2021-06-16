8ffc3f9527fcaeeebbff35cb8439b45d7801fddd
DinhManhTri <Tridmth2008060@fpt.edu.vn>
2021-06-16 15:16:06 +0700

    update code

---
+++ Ex2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var highSchoolTeacher = new HighSchoolTeacher
+            {
+                IndentityNumber = "ID001",
+                Name = "Spring Hero",
+                BaseSalary = 100,
+                Level = 1,
+                Bonus = 100,
+                IsSenior = false,
+                HighSchoolCode = "HS001"
+            };
+            Console.WriteLine(highSchoolTeacher.CalculateSalary());
+            var universityTeacher = new UniversityTeacher
+            {
+                IndentityNumber = "ID002",
+                Name = "Manh Tri",
+                BaseSalary = 60000,
+                Level = 1,
+                Bonus = 9000,
+                IsSenior = true,
+                EnglishLevel = 2,
+                UniversityCode = "UN001"
+            };
+            Console.WriteLine(universityTeacher.CalculateSalary());
+        }
+    }
+}
